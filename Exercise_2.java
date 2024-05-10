315fb0daabadd966cd8178669077353b7fac46ce
Rishav Gupta <gupta.rishav2608@gmail.com>
2024-05-10 07:00:22 +0530

    Done precourse 1

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,4 +1,4 @@
-public class StackAsLinkedList { 
+class StackAsLinkedList { 
   
     StackNode root; 
   
@@ -8,7 +8,8 @@ public class StackAsLinkedList {
   
         StackNode(int data) 
         { 
-            //Constructor here 
+            this.data = data;
+            this.next = null;
         } 
     } 
     
@@ -16,11 +17,23 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+        if (root == null){
+            return true;
+        } else {
+            return false;
+        }
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+        StackNode newNode = new StackNode(data);
+        if(isEmpty()) {
+            root = newNode;
+        } else {
+            newNode.next = root;
+            root = newNode;
+        }
     } 
   
     public int pop() 
@@ -28,11 +41,25 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+        if(isEmpty()) {
+            return 0;
+            // System.out.println("Stack Underflow");
+        } else {
+            int value = root.data;
+            root = root.next;
+            return value;
+        }
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+        if(isEmpty()) {
+            return 0;
+            // System.out.println("Stack Underflow");
+        } else {
+            return root.data;
+        }
     } 
   
 	//Driver code
--- Exercise_3.java
+++ Exercise_3.java
@@ -1,8 +1,6 @@
-import java.io.*; 
-  
 // Java program to implement 
 // a Singly Linked List 
-public class LinkedList { 
+class LinkedList { 
   
     Node head; // head of list 
   
@@ -18,6 +16,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+            this.data = d;
+            this.next = null;
         } 
     } 
   
@@ -34,7 +34,17 @@ public class LinkedList {
 
             // Insert the new_node at last node 
         // Return the list by head 
-        
+        Node newNode = new Node(data);
+        Node temp = list.head;
+        if(list.head == null) {
+            list.head = newNode;
+        } else {
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        return list;
     } 
   
     // Method to print the LinkedList. 
@@ -45,6 +55,16 @@ public class LinkedList {
             // Print the data at current node 
        
             // Go to next node 
+
+        Node temp = list.head;
+        if(list.head == null) {
+            System.out.println("No data");
+        } else {
+            while(temp != null) {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+        }
     } 
    
     // Driver code 
